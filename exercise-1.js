d68014c50536298cd1413f52ae56f000634821fa
MelofunkCodes <myeoh27@gmail.com>
2017-01-25 19:30:40 +0000

    First commit. Added exercise 1 and updated package.json

---
+++ exercise-1.js
@@ -0,0 +1,50 @@
+var mysql = require('promise-mysql');
+var Table = require('cli-table');
+var colors = require('colors');
+
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'myeoh27',
+    password: '',
+    database: 'addressbook',
+    connectionLimit: 10
+});
+
+
+//===========FUNCTION DECLARATION===============
+function getDatabases() {
+
+    return connection.query("SHOW DATABASES")
+        .then(function(result) {
+            //console.log(result);
+            var dbList = result.map(function(eachDatabase) {
+                return [eachDatabase.Database]; //getting the names of each database inside each object, making each name its own array
+            }); //db_list will be an array of arrays
+
+            console.log("db_list: ", dbList);
+
+            var table = new Table({
+                head: ['DATABASES'.blue.bold.underline],
+                colWidths: [20]
+            });
+
+            dbList.forEach(function(eachArray) {
+                table.push(eachArray);
+            });
+
+            return table.toString();
+
+        });
+}
+
+
+//===========CALLING FUNCTION===================
+getDatabases()
+    .then(function(result) {
+        console.log(result);
+
+        connection.end();
+    })
+    .catch(function(error) {
+        console.log("Error happened. ", error);
+    });
